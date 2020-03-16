6bb79ea9bfa2e7ef9e7c3acb2873b28f860b8a9b
Pichet Boonpheng <pichet.boonpheng@gmail.com>
2020-03-16 21:58:37 +0700

    02

--- src/component/map.js
+++ src/component/map.js
@@ -11,16 +11,27 @@ export default function Maps(props) {
       const datas = res.data
       datas.forEach(doc => {
         const location = doc.data.location
-        // const gel = doc.data.gel
-        // const mask = doc.data.mass
         const name = doc.data.name
         console.log(doc.data)
+        var imgSrc = ""
+        if (doc.data.gel === 0 && doc.data.mass === 0) {
+          imgSrc = "https://firebasestorage.googleapis.com/v0/b/covid19-1013d.appspot.com/o/hm_Red.png?alt=media&token=1c543ba0-5260-4344-85bc-c9fff8bf7bcf"
+        }
+        else if (doc.data.gel !== 0 && doc.data.mass === 0) {
+          imgSrc = "https://firebasestorage.googleapis.com/v0/b/covid19-1013d.appspot.com/o/hm_GreedRed.png?alt=media&token=a8b4204d-1e1f-496e-a511-84ccf79b62f9"
+        }
+        else if (doc.data.gel === 0 && doc.data.mass !== 0) {
+          imgSrc = "https://firebasestorage.googleapis.com/v0/b/covid19-1013d.appspot.com/o/hm_RedGreen.png?alt=media&token=422fb07b-894b-42a7-a422-25bd8812c595"
+        }
+        else {
+          imgSrc = "https://firebasestorage.googleapis.com/v0/b/covid19-1013d.appspot.com/o/hm_Green.png?alt=media&token=3bb82f1c-d8c7-48d2-8976-7860621a0243"
+        }
         var marker = new longdo.Marker({ lon: location.lng, lat: location.lat }, {
           title: name,
           icon: {
-            html: `
-              <h3 style="background-color: powderblue;padding:10px"> Hell </h3>
-            `,
+            html: `<img 
+            src="${imgSrc}"
+            style="width:auto;height:100px;transform: translate(0,-50%)" />`,
             offset: { x: 12, y: 45 }
           },
           popup: {
@@ -35,10 +46,10 @@ export default function Maps(props) {
     })
 
     map.Ui.DPad.visible(false)
-    map.Ui.Zoombar.visible(false)
+    map.Ui.Zoombar.visible(true)
     map.Ui.Geolocation.visible(false)
     map.Ui.Toolbar.visible(false)
-    map.Ui.LayerSelector.visible(false)
+    map.Ui.LayerSelector.visible(true)
     map.Ui.Fullscreen.visible(false)
     map.Ui.Scale.visible(false)
 
